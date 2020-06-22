ae8ed3b07d1b66d080a9d4620058ec8dbc933944
Jamie Nevada <admin@cryogena.net>
2020-06-22 05:18:26 -0600

    Fix prefix addition

--- src/main/java/io/github/spigotjs/managers/CommandManager.java
+++ src/main/java/io/github/spigotjs/managers/CommandManager.java
@@ -7,12 +7,12 @@ import io.github.spigotjs.utils.ScriptBukkitCommand;
 import io.github.spigotjs.utils.ScriptCommand;
 
 public class CommandManager {
-
 	public void on(String name, String prefix, Consumer<ScriptCommand> consumer) {
-		if (prefix == undefined) { prefix  = name; }
 		ScriptBukkitCommand command = new ScriptBukkitCommand(name, consumer);
 		SpigotJSReloaded.getInstance().getScriptManager().getScriptBukkitCommands().add(command);
 		SpigotJSReloaded.getInstance().getScriptManager().getCommandMap().register(name, prefix, command);
 	}
-
+	public void on(String name, Consumer<ScriptCommand> consumer) {
+		on(name, null, consumer);
+	}
 }
